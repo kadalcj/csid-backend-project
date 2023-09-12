07629731e864d30b158539a35af37e9bf174d1ae
kadalcj <ilhamwahyua@gmail.com>
2023-09-12 21:00:18 +0700

    Prisma Scheme & Docker

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+// import { PrismaClient } from '@prisma/client'
 
 const app = express()
-const port = 6969
+// const prisma = new PrismaClient()
 
+dotenv.config()
+const port = process.env.PORT
+
+/**Root Route */
 app.get('/', (_, res) => {
     res.send('Root')
 })
 
+/**Listen on port */
 app.listen(port, () => {
     console.log(`Listen on port ${port}`)
 })
